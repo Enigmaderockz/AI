23aeef5adad890aabb72a7b304fb34914c854d00
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2023-04-20 01:53:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 import os
 
 # Define the directory path where the .cfg files are located
@@ -11,8 +10,15 @@ for filename in os.listdir(dir_path):
         with open(file_path, "r") as file:
             # Read each line of the file
             for line in file:
-                # Search for the string "EDW1_RBG_STAGE" or "EDW1_RBE_STAGE"
-                if "EDW1_RBG_STAGE" in line or "EDW1_RBE_STAGE" in line:
-                    # If the search string is found, search for the string "TABLE_NAME"
-                    if "TABLE_NAME" in line:
-                        print(f"File: {filename} - Line: {line}")
+                # Search for the string "EDW1_RBG_STAGE"
+                if "EDW1_RBG_STAGE" in line:
+                    # If the search string is found, extract the prefix from the line
+                    prefix = line.split(".")[0]
+                    # Search for the string "TABLE_NAME" in the file
+                    file.seek(0)  # Return to the beginning of the file
+                    for line in file:
+                        if f"{prefix}.TABLE_NAME" in line:
+                            # If the search string is found, print the line
+                            print(f"File: {filename} - Line: {line}")
+                            break
+                    break  # Stop searching for "EDW1_RBG_STAGE" after the first occurrence

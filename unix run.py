16c2e491e74d0b7ea29606c089a73b50a3ef9c7a
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2023-05-11 17:55:32 +0530

    Create unix run.py

---
+++ unix run.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Set the path to your shell script
+script_path = os.path.join(current_directory, 'script.sh')
+
+# Make sure the script has execute permissions
+subprocess.run(['chmod', '+x', script_path])
+
+# Execute the shell script
+result = subprocess.run([script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+# Check if the script executed successfully
+if result.returncode == 0:
+    print("Script executed successfully.")
+    print("Output:", result.stdout)
+else:
+    print("Script execution failed.")
+    print("Error:", result.stderr)

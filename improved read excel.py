50f8736fd7dd0e1625d1edf8e820ff0b526c2848
Abhinandan Mehandiratta <39618909+Enigmaderockz@users.noreply.github.com>
2023-05-29 23:30:47 +0530

    Update improved read excel.py

--- improved read excel.py
+++ improved read excel.py
@@ -24,3 +24,31 @@ def read_file(file_path):
     else:
         raise ValueError("Unsupported file format")
     return rows
+
+
+def read_file(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    if file_extension == ".csv":
+        with open(file_path, "r") as f:
+            reader = csv.DictReader(f)
+            rows = [row for row in reader]
+    elif file_extension == ".xlsx":
+        wb = openpyxl.load_workbook(file_path, read_only=True, data_only=True)
+        rows = []
+        for sheet_name in wb.sheetnames:
+            sheet = wb[sheet_name]
+            header = [cell.value for cell in sheet[1]]
+            sheet_rows = []
+
+            # Handle merged cells
+            for merged_cell in sheet.merged_cells.ranges:
+                merged_cell.shift(0, 1, unmerge=True)
+
+            for row in sheet.iter_rows(min_row=2, values_only=True, skip_null_rows=False):
+                row_dict = {header[i]: value for i, value in enumerate(row)}
+                sheet_rows.append(row_dict)
+            rows.extend(sheet_rows)
+    else:
+        raise ValueError("Unsupported file format")
+    return rows
+
